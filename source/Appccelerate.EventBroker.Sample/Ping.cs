f1094164adea5d2c29f42446ca8701f20605594b
Urs Enzler <urs.enzler@gmail.com>
2014-01-03 15:00:25 +0100

    moved code in for sample application from old repository

---
+++ source/Appccelerate.EventBroker.Sample/Ping.cs
@@ -0,0 +1,117 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Ping.cs" company="Appccelerate">
+//   Copyright (c) 2008-2013   Licensed under the Apache License, Version 2.0 (the "License");   you may not use this file except in compliance with the License.   You may obtain a copy of the License at       http://www.apache.org/licenses/LICENSE-2.0   Unless required by applicable law or agreed to in writing, software   distributed under the License is distributed on an "AS IS" BASIS,   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   See the License for the specific language governing permissions and   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.EventBroker.Sample
+{
+    using System;
+    using System.Windows.Forms;
+
+    using Appccelerate.Events;
+
+    /// <summary>
+    /// The ping form.
+    /// </summary>
+    public partial class Ping : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ping"/> class.
+        /// </summary>
+        public Ping()
+        {
+            this.InitializeComponent();
+            
+            Application.ThreadException += (sender, args) =>
+                this.BurstText.Text = "caught exception " + args.Exception;
+        }
+
+        /// <summary>
+        /// Occurs when the ping UI from UI thread is fired.
+        /// </summary>
+        [EventPublication(EventTopics.PingUIFromUIThread)]
+        public event EventHandler UiFromUiEvent;
+
+        /// <summary>
+        /// Occurs when the ping UI from asynchronous is fired.
+        /// </summary>
+        [EventPublication(EventTopics.PingUIFromAsync)]
+        public event EventHandler UiFromAsyncEvent;
+
+        [EventPublication(EventTopics.BurstPing, HandlerRestriction.Asynchronous)]
+        public event EventHandler<EventArgs<int>> BurstEvent;
+
+        [EventPublication(EventTopics.PingSubscriberException)]
+        public event EventHandler Exception;
+
+        /// <summary>
+        /// Handles the pong UI from UI thread event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments</param>
+        [EventSubscription(EventTopics.PongUIFromUIThread, typeof(Handlers.OnUserInterface))]
+        public void HandleUiFromUi(object sender, EventArgs e)
+        {
+            this.FeedbackLabel.Text = "UI from UI thread called.";
+        }
+
+        /// <summary>
+        /// Handles the pong UI from asynchronous event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        [EventSubscription(EventTopics.PongUIFromAsync, typeof(Handlers.OnUserInterfaceAsync))]
+        public void HandleUiFromAsync(object sender, EventArgs e)
+        {
+            this.FeedbackLabel.Text = "UI from asynchronous.";
+        }
+
+        [EventSubscription(EventTopics.BurstPong, typeof(Handlers.OnUserInterfaceAsync))]
+        public void HandleBurst(int index)
+        {
+            this.BurstText.Text += " " + index;
+        }
+
+        /// <summary>
+        /// Handles the load event.
+        /// </summary>
+        /// <param name="e">The load event argument.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.timer.Enabled = true;
+        }
+
+        private void CallUIFromUIButton_Click(object sender, EventArgs e)
+        {
+            this.UiFromUiEvent(this, EventArgs.Empty);
+        }
+
+        private void UIFromAsyncButton_Click(object sender, EventArgs e)
+        {
+            this.UiFromAsyncEvent(this, EventArgs.Empty);
+        }
+
+        private void ExceptionButton_Click(object sender, EventArgs e)
+        {
+            this.Exception(this, EventArgs.Empty);
+        }
+
+        private void BurstButton_Click(object sender, EventArgs e)
+        {
+            this.BurstText.Text = string.Empty;
+
+            for (int i = 0; i < 50; i++)
+            {
+                this.BurstEvent(this, new EventArgs<int>(i));
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.Time.Text = DateTime.Now.ToString("hh:mm:ss.fff");
+        }
+    }
+}
---
+++ source/Appccelerate.EventBroker.Sample/Pong.cs
@@ -0,0 +1,79 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Pong.cs" company="Appccelerate">
+//   Copyright (c) 2008-2013   Licensed under the Apache License, Version 2.0 (the "License");   you may not use this file except in compliance with the License.   You may obtain a copy of the License at       http://www.apache.org/licenses/LICENSE-2.0   Unless required by applicable law or agreed to in writing, software   distributed under the License is distributed on an "AS IS" BASIS,   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   See the License for the specific language governing permissions and   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.EventBroker.Sample
+{
+    using System;
+    using System.Threading;
+    using Appccelerate.EventBroker.Handlers;
+    using Appccelerate.Events;
+
+    /// <summary>
+    /// Pong class.
+    /// </summary>
+    public class Pong
+    {
+        /// <summary>
+        /// Occurs when the pong UI from UI thread is fired.
+        /// </summary>
+        [EventPublication(EventTopics.PongUIFromUIThread)]
+        public event EventHandler UiFromUiEvent;
+
+        /// <summary>
+        /// Occurs when the pong UI from async is fired.
+        /// </summary>
+        [EventPublication(EventTopics.PongUIFromAsync)]
+        public event EventHandler UiFromAsyncEvent;
+
+        [EventPublication(EventTopics.BurstPong, HandlerRestriction.Asynchronous)]
+        public event EventHandler<EventArgs<int>> Burst; 
+
+        /// <summary>
+        /// Handles a ping.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments</param>
+        [EventSubscription(EventTopics.PingUIFromUIThread, typeof(Handlers.OnPublisher))]
+        public void HandlePingUiFromUiThread(object sender, EventArgs e)
+        {
+            Wait();
+            this.UiFromUiEvent(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Handles an asynchronous ping.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        [EventSubscription(EventTopics.PingUIFromAsync, typeof(Handlers.OnBackground))]
+        public void HandlePingUiFromAsync()
+        {
+            Wait();
+            this.UiFromAsyncEvent(this, EventArgs.Empty);
+        }
+
+        [EventSubscription(EventTopics.BurstPing, typeof(Handlers.OnBackground))]
+        public void HandleBurst(EventArgs<int> e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Thread.Sleep(50 - i);
+                this.Burst(this, new EventArgs<int>(i * e.Value));
+            }
+        }
+
+        [EventSubscription(EventTopics.PingSubscriberException, typeof(OnPublisher))]
+        public void HandleException()
+        {
+            throw new Exception("evil subscriber");
+        }
+
+        private static void Wait()
+        {
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
