109dd139fbd07293bc9adac724f542427d064c43
Urs Enzler <urs.enzler@gmail.com>
2015-01-13 16:35:28 +0100

    fixed referenced version of Fundamentals, updated copyright and dependencies

--- source/Appccelerate.EventBroker/Internals/Publications/CodePublication.cs
+++ source/Appccelerate.EventBroker/Internals/Publications/CodePublication.cs
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------
 // <copyright file="CodePublication.cs" company="Appccelerate">
-//   Copyright (c) 2008-2014
+//   Copyright (c) 2008-2015
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
